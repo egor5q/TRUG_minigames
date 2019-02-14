e70571cd24531659dd9f404521cbf7d206614aaf
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-14 11:26:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,10 @@ def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
     return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
                                  parse_mode=parse_mode)  
 
-#client=MongoClient(os.environ['database'])
-#db=client.trugminigames
-#users=db.users
+
+client3=MongoClient(os.environ['database2'])
+db2=client3.trug
+trugusers=db2.users
 
 allgames=['donkey']
 currentgame=[]
